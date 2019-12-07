a3c102ec96655d1b9ee426199999f4be0a45f438
Asif Kamal <asif_kamal@hotmail.com>
2019-12-07 17:04:57 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
   element_index = 0
     #binding.pry
     while element_index < directors_database[0][:movies].length do
-      puts "#{directors_database[0][:movies][element_index][:title]} \n"
+      puts "#{directors_database[0][:movies][element_index][:title]}\n"
       element_index += 1
     end
 end
